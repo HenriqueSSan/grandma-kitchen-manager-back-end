fd96670294176846cd188c1cf460128f91058040
HenriqueSSan <hsstudentwork@gmail.com>
2025-11-11 02:29:25 -0300

    feat: added body parser and cors in app

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+import cors from "cors"
+import express from "express"
+import body_parser from "body-parser"
+
+const app =  express()
+
+
+app.use(body_parser.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cors({ origin: "http://localhost:5173" }))
