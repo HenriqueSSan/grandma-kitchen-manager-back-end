6ceebac15ff2fe7e15dbba670e13000e9a76e8c5
HenriqueSSan <hsstudentwork@gmail.com>
2025-11-11 10:57:08 -0300

    feat: resolved import app in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import body_parser from "body-parser"
 
 const app =  express()
 
-
 app.use(body_parser.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(cors({ origin: "http://localhost:5173" }))
+
+export { app }
